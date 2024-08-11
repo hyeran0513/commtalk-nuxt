807d2b93af6df9343b80dc97b0396e2dce1e1869
hyeran0513 <hr_0513@naver.com>
2024-08-11 15:44:56 +0900

    회원 프로필 업로드, 조회 api 반영

--- components/layout/sideBar.vue
+++ components/layout/sideBar.vue
@@ -4,6 +4,25 @@
       <NuxtLink to="/" class="logo"></NuxtLink>
     </div>
 
+    <div class="profile">
+      <div class="profile-img" :style="{background: `#f8f8f8 url(${ profile?.fileUrl }) no-repeat center/auto 100%`}"></div>
+
+      <button
+          type="button"
+          class="btn-setting"
+          @click.prevent="triggerFileInput"
+      >
+        <i class="icon-settings" />
+      </button>
+
+      <input
+          type="file"
+          ref="fileInput"
+          style="display: none"
+          @change="uploadProfile"
+      />
+    </div>
+
     <div class="menu">
       <div v-for="(item, index) in menuItems" :key="index" class="menu-item">
         <NuxtLink :to="item.link" 
@@ -25,7 +44,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { useLocalStorage } from "@vueuse/core";
+
+const token = useLocalStorage('token', '');
 
 const menuItems = ref([
 { 
@@ -73,6 +95,57 @@ const activeIndex = ref(null)
 const toggle = (index) => {
   activeIndex.value = activeIndex.value === index ? null : index
 }
+
+/* 회원 프로필 사진 업로드 */
+const fileInput = ref();
+
+const triggerFileInput = () => {
+  fileInput.value.click();
+}
+
+const uploadProfile = async (event) => {
+  const fileData = event.target.files[0];
+
+  if (!fileData) return;
+
+  const formData = new FormData();
+  formData.append('file', fileData);
+
+  try {
+    const response = await fetch(`/api/v1/files/profile`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+      body: formData,
+    });
+
+    if (response.ok) {
+      console.log('성공');
+      const result = await response.json();
+      console.log(result);
+    } else {
+      console.log('성공X');
+    }
+  } catch (error) {
+    console.error('에러:', error);
+  }
+}
+/* END: 회원 프로필 사진 업로드 */
+
+// 회원 프로필 사진 조회
+const { data: profile, execute: exeProfile } = await useAsyncData('profile',
+  () => $fetch(`/api/v1/files/profile`, {
+    headers: {
+      'Authorization': `Bearer ${token.value}`,
+      'Content-Type': 'application/json'
+    }
+  })
+);
+
+onMounted(() => {
+  exeProfile();
+})
 </script>
 
 <style lang="scss" scoped>
