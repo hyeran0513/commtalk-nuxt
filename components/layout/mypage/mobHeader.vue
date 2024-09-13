bb1623714e8919136f13a20f2606c9c6602bc865
hyeran0513 <hr_0513@naver.com>
2024-09-13 21:33:58 +0900

    마이페이지 모바일 반응형 작업

---
+++ components/layout/mypage/mobHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div class="header-inner">
+      <NuxtLink to="/" class="logo"></NuxtLink>
+
+      <div class="btn-wrap">
+        <button
+            type="button"
+            class="btn-logout"
+            title="로그아웃"
+            @click="logout()"
+        >
+          <i class="icon-log-out" />
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import {useLocalStorage} from "@vueuse/core";
+
+const token = useLocalStorage('token', '');
+const route = useRoute();
+const router = useRouter();
+
+// 로그아웃
+const logout = () => {
+  token.value = '';
+  router.push('/login');
+};
+</script>
+
+<style lang="scss" scoped>
+@import url('~/assets/scss/components/layout/header.scss');
+</style>
+
