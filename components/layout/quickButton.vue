01e8fb3f2a0666bebe5e1dee1e2bbac97574f19a
hyeran0513 <hr_0513@naver.com>
2024-09-13 15:32:18 +0900

    pinia 사용하여 뒤로가기 버튼 구현 및 swiper.js로 모바일 카테고리 추가

---
+++ components/layout/quickButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="quick-buttons">
+    <button
+        type="button"
+        class="top-btn"
+    >
+      <i class="icon-arrow-up" />
+    </button>
+  </div>
+</template>
